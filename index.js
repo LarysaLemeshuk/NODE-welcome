796412eddd21e6219837a199104063237ce5ee11
Larysa Lemeshuk <larusalemeshuk@gmail.com>
2025-05-18 14:59:07 +0200

    AppendFile

--- index.js
+++ index.js
@@ -4,6 +4,14 @@ const fs = require('fs').promises;
 fs.readFile('./text.txt', 'utf-8');
 
 then((content) => {
-  const newContent = `OLD content: ${content} and NEW content: 'Helo world'`;
-  fs.writeFile('./newfile.txt', newContent, 'utf8');// перезаписує файли!!!
+  const newContent = `APPEND OLD content: ${content} and NEW content: 'Helo world'`;
+  //fs.writeFile('./newfile.txt', newContent, 'utf8');// перезаписує файли!!!
+  fs.appendFile('./newfile.txt', newContent, 'utf8'); // дописує файли
 });
+
+/*
+Задача: замінити якусь частину файлу
+1. Читаємо ВЕСЬ файл через readFile -› ВЕСЬ вміст файлу
+2. Якось змінюємо вмість через JS -> ту частину вмісту файлу, яка нам потрібна
+3. Перезаписати цільовий файл через writeFile тими даними, які ви отримали у п. 2
+*/
